307c495c1650d659bbdd795825d782290c86e180
José Miguel Loureiro <jose.miguel.loureiro@gmail.com>
2017-09-20 12:04:41 -0500

    Scan Input
    Added Scan of input and print of array with input;

--- src/SortingBalls.java
+++ src/SortingBalls.java
@@ -9,8 +9,20 @@ public class SortingBalls {
         Scanner sc = new Scanner(System.in);//scanner to read input
         int in;//input to be read from Standard Input
         Vector<Integer> vector = new Vector<Integer>();//vector to manipulate input
-        int n[] = new int[6];// array with results;
+        Integer n[] = new Integer[6];// array with results;
 
+        for (int i=0;i<6;i++){
+            in = sc.nextInt();
+            //System.out.println(in);
+            vector.add(in);
+        }
+
+        //convert and print result
+        n = (Integer[]) vector.toArray(new Integer[6]);
+        for (int i=0;i<6;i++){
+            System.out.print(n[i]+" ");
+        }
+        System.out.println();
 
     }
 }
