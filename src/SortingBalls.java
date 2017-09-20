a715dc07ae30532395a776f2281c7bde9af35f68
José Miguel Loureiro <jose.miguel.loureiro@gmail.com>
2017-09-20 11:55:30 -0500

    Added Variables
    Added Variables necessary for manipulation;

--- src/SortingBalls.java
+++ src/SortingBalls.java
@@ -5,5 +5,12 @@
 import java.util.*;
 
 public class SortingBalls {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);//scanner to read input
+        int in;//input to be read from Standard Input
+        Vector<Integer> vector = new Vector<Integer>();//vector to manipulate input
+        int n[] = new int[6];// array with results;
 
+
+    }
 }
